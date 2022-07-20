c3b4b83a0e32d6ee9b7a442a1a039b06e8f0e4d8
Lilian Alves Durães <duraes.lilian@gmail.com>
2022-07-19 21:08:56 -0300

    Teste Numbers

--- src/average.js
+++ src/average.js
@@ -20,5 +20,6 @@ const average = (array) => {
     }
   }
 }
+return total;
 
 module.exports = average;
--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,9 +16,22 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-  expect [1, 2, 3, 4, 5].toEqual(true);
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers(1)).toEqual(true);
+    expect(numbers('string')).toBe(false);
+   
+    /* Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true*/
+    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+    /* Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false*/
+    expect(numbers([1, 2, '3', 4, 5])).toEqual(false);
+    /* Escreva um teste em que a função recebe [1, 'a', 3] e retorna false*/
+    expect(numbers(1, 'a', 3)).toBe(false);  
+    /* Escreva um teste em que a função recebe [' '] e retorna false*/
+    expect(numbers()).toEqual(false);
+    
+   
+    
+   
+    
   });
 });
+
